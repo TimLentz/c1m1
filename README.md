ae1929d54382950b51e686c00ff5be4a91cacf6c
Tim Lentz <tlent234@live.kutztown.edu>
2023-05-03 22:06:15 -0400

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,4 @@
+/* Add Author and Project Details here */
+Author: Tim Lentz
+Language: C
+Description: This program contains functions that can analyze an arrau of unsidnged char data and create a report on the maximum, minimum, mean, and median of the data set.
---
+++ stats.h
@@ -0,0 +1,43 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c
+ * @brief This program analiyzes an array of data and creates 
+ * a report
+ *
+ * This program contains functions that can analyze an array
+ * of unsidnged char data and create a report on the maximum, 
+ * minimum, mean, and median of the data set.
+ *
+ * @author Tim Lentz
+ * @date 5/3/2023
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
